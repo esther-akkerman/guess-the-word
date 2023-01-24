7a1e67da0578d64d1643e8c49fbd993180c61a6a
akkerman_e <estherakkerman@hotmail.com>
2023-01-24 16:13:35 -0600

    Added all needed variables to manipulate the DOM

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordProgress = document.querySelector(".word-in-progress");
+const guessRemainPar = document.querySelector(".remaining");
+const guessRemain = document.querySelector("span");
+const message = document.querySelector(".message");
+const againButton = document.querySelector(".play-again");
+
+const word = "magnolia";
+
+const showWord = function(word) {
+    const wordLength = word.length;
+       
+}
+
+showWord(word);
