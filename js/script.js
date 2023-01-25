b03309c7d8b8526225b285061f48403750e0eac7
akkerman_e <estherakkerman@hotmail.com>
2023-01-25 16:21:20 -0600

    Added funtion to show guessed letters

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 const guessedLetters = document.querySelector(".guessed-letters");
-const guessedLetterArray = [];
 const guessButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
 const wordProgress = document.querySelector(".word-in-progress");
@@ -9,9 +8,12 @@ const message = document.querySelector(".message");
 const againButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetterArray = [];
 
 const showWord = function(word) {
+    // placeholder for showing how far the word is guessed
     const wordLength = word.length;
+    // came up with this solution, it looks strange, but it does work. Good creative problem solving, but I think I'll learn better ways!
     const dots = ["●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●","●"]
     dots.splice(wordLength, (40-wordLength));
     wordProgress.innerText = dots.join("");
@@ -20,18 +22,19 @@ const showWord = function(word) {
 showWord(word);
     
 guessButton.addEventListener("click", function(e) {
+    // all functions happening when the guess button is clicked
     e.preventDefault();
     message.innerText = "";
     inputLetter = letterInput.value;
     letterInput.value = "";
-    valInput(inputLetter);
-    const returnLetter = valInput(inputLetter); //Add a conditional block somewhere that only letters are output.
+    const returnLetter = valInput(inputLetter);
     if (returnLetter !== undefined) {
         makeGuess(returnLetter);
     };
 });
 
 const valInput =  function(inputLetter) {
+    // sets condiontal blocks to make sure the input is a letter
     const acceptedLetters = /[a-zA-Z]/;
     if (inputLetter === "") {
         message.innerText = "There is no input, please fill in a letter from A to Z.";
@@ -46,12 +49,23 @@ const valInput =  function(inputLetter) {
 };
 
 const makeGuess = function(letter) {
-    letter.toUppercase;
+    // adds the new guessed letter to the array
+    letter = letter.toUpperCase();
     if (guessedLetterArray.includes(letter) === true) {
         message.innerText = "You already guessed this letter before, try a new letter.";
     } else {
         guessedLetterArray.push(letter);
+        updateGuess();
         console.log(guessedLetterArray);
     }
+};
+
+const updateGuess = function() {
+    wordProgress.innerHTML = "";
+    for (let item of guessedLetterArray) {
+        let li = document.createElement("li");
+        li.innerText = item;
+        guessedLetters.append(li);
+    };
 };
 
